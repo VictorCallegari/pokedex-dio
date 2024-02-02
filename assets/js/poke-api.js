36101bbe5fdfcb475974a8962b13a457132181cc
Victor Callegari <agro.callegari@hotmail.com>
2024-02-01 22:28:15 -0300

    ajustando API

---
+++ assets/js/poke-api.js
@@ -0,0 +1,11 @@
+// Função que abtrai o consumo do http
+
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .catch((error) => console.log(error))
+}
