0a8603947efa861a7040770dce8046fc4003a0a4
Victor Callegari <agro.callegari@hotmail.com>
2024-02-03 11:39:10 -0300

    atualização no projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,50 @@
+const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonList = document.getElementById('pokemonList');
+const limit = 6;
+let offset = 0;
 
 function convertPokemonToLi(pokemon) {
+    const paddedNumber = String(pokemon.number).padStart(4, '0');
     return `
-            <li class="pokemon ${pokemon.type}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
-
-                <div class="detail">
-                     <ol class="types">
-                         ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
-                     </ol>
-
-                    <img src="${pokemon.photo}"
-                         alt="${pokemon.name}">
-                </div>
-
-            </li>`
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${paddedNumber}</span>
+            <span class="name">${pokemon.name}</span>
+    
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+    
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+            </div>
+        </li>`;
 }
 
-const pokemonList = document.getElementById('pokemonList');
+function appendPokemonToDOM(pokemons) {
+    const newHtml = pokemons.map(convertPokemonToLi).join('');
+    pokemonList.innerHTML += newHtml;
+}
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
-})
+function loadPokemonItems(offset, limit) {
+    pokeApi.getPokemons(offset, limit)
+        .then((pokemons = []) => {
+            appendPokemonToDOM(pokemons);
+        })
+        .catch((error) => {
+            console.error('Error loading Pokemon items:', error);
+            // Trate o erro conforme necessário (por exemplo, exibindo uma mensagem para o usuário)
+        });
+}
+
+loadPokemonItems(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+    loadPokemonItems(offset, limit);
+});
+
+
+
+
 
 
